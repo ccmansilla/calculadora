77d26af5714c36c8eb70e0bb7d34cc78383a7cf9
ccmansilla <carloscmansilla@gmail.com>
2022-09-24 07:33:17 -0300

    funciones en español y mejoro estilos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 /* display */
 const display = document.getElementById("resultado");
+
+/* acumulador de funciones */
 const funciones = document.getElementById("funciones");
 
 /* botones de teclado */
@@ -8,59 +10,64 @@ const teclado = document.querySelectorAll(".btnCalc");
 /* memoria */
 let memoria = 0;
 
+/* constantes matematicas */
+const e = Math.E;
+const π = Math.PI;
+
 teclado.forEach(function(btn) {
     btn.addEventListener('click', function() {
         switch(btn.value) {
             case 'AC':
                 display.innerHTML = '';
+                funciones.innerHTML = '';
                 break;
             case 'MR':
                 display.innerHTML = memoria;
                 break;
             case 'x²':
-                display.innerHTML += 'Math.pow(';
+                display.innerHTML += 'potencia(';
                 funciones.innerHTML += ',2)';
                 break;                
             case 'x³':
-                display.innerHTML += 'Math.pow(';
+                display.innerHTML += 'potencia(';
                 funciones.innerHTML += ',3)';
                 break;   
             case 'xⁿ':
                 funciones.innerHTML = ',' + display.innerHTML + ')';
-                display.innerHTML = 'Math.pow(';
+                display.innerHTML = 'potencia(';
                 break;  
             case '√':
-                display.innerHTML += 'Math.sqrt(';
+                display.innerHTML += 'raiz(';
                 funciones.innerHTML += ')';
                 break;     
             case '√n':
-                funciones.innerHTML = ',' + display.innerHTML + ')';
-                display.innerHTML = 'Math.sqrt(';
+                funciones.innerHTML =  display.innerHTML + ')';
+                display.innerHTML = 'raiz(';
                 break;         
             case 'sin':
-                display.innerHTML += 'Math.sin(';
+                display.innerHTML += 'seno(';
                 funciones.innerHTML += ')';
                 break;
             case 'cos':
-                display.innerHTML += 'Math.cos(';
+                display.innerHTML += 'coseno(';
                 funciones.innerHTML += ')';
                 break;
             case 'tg':
-                display.innerHTML += 'Math.tan(';
+                display.innerHTML += 'tangente(';
                 funciones.innerHTML += ')';
                 break;
             case 'π':
-                display.innerHTML += 'Math.PI ';
+                display.innerHTML += 'π';
                 break;
             case 'e':
-                display.innerHTML += 'Math.E ';
+                display.innerHTML += 'e';
                 break;
             case 'ln':
-                display.innerHTML += 'Math.log(';
+                display.innerHTML += 'ln(';
                 funciones.innerHTML += ')';
                 break;
             case 'log':
-                display.innerHTML += 'Math.log10(';
+                display.innerHTML += 'log(';
                 funciones.innerHTML += ')';
                 break;
             case 'x!':
@@ -87,4 +94,32 @@ function factorial(n) {
         return n * factorial(n - 1);
     }
     return 1;
+}
+
+function seno(n) {
+    return Math.sin(n);
+}
+
+function coseno(n) {
+    return Math.cos(n);
+}
+
+function tangente(n) {
+    return Math.tan(n);
+}
+
+function potencia(b, e) {
+    return Math.pow(b,e)
+}
+
+function raiz(n) {
+    return Math.sqrt(n);
+}
+
+function ln(n) {
+    return Math.log(n);
+}
+
+function log(n) {
+    return Math.log10(n);
 }
